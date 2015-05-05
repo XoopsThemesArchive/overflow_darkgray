5518fd348aaed7eaca29326bd25cfd429eea697b
mambax7 <mambax7@gmail.com>
2015-05-05 09:23:20 -0400

    Original files

---
+++ overflow_darkgray/manifesto.ini.php
@@ -0,0 +1,20 @@
+<?php
+/**
+
+[Manifesto]
+Name="overflow_darkgray"
+Depends=Legacy_RenderSystem
+Url="http://www.expiece.com/html/"
+Version="1.0"
+
+[Theme]
+RenderSystem=Legacy_RenderSystem
+Format="XOOPS2 Legacy Style"
+Author=hi-debow @ ExPiece.
+ScreenShot="screenshot.png"
+Description="irregular 3 columns fixed width CSS layout style theme"
+
+License="Attribution-Noncommercial 2.1 Japan"
+
+*/
+?>
